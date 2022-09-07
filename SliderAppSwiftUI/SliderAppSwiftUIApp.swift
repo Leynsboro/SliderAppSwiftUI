6d2c84b1dc008e6e2f6d9ad6435bc410c9592b90
Ilya <leynsboro@gmail.com>
2022-09-07 23:16:33 +0300

    Initial Commit

---
+++ SliderAppSwiftUI/SliderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppSwiftUIApp.swift
+//  SliderAppSwiftUI
+//
+//  Created by Илья Гусаров on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
